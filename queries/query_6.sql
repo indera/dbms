8732eaafac175c58d2a2a4a0f7410477e2ced37b
Mukul Chand Yadav <mchand.yadav@ufl.edu>
2020-04-15 10:46:52 -0400

    Amend query 6
    Commit made on behalf of Dimitrios and Shangde

--- queries/query_6.sql
+++ queries/query_6.sql
@@ -1,23 +1,20 @@
+-- Shangde:
 -- Query 1: group the loan by district and calculate the total amount of loan on each date in each district
-
-
-SELECT
-    SUM(LOAN.amount) FROM dmelisso.LOAN LOAN 
-    JOIN dmelisso.account ACCOUNT on ACCOUNT.account_id = LOAN.account_id
-    JOIN dmelisso.DISTRICT on DISTRICT.district_id = ACCOUNT.district_id
-GROUP
-    BY LOAN.date, DISTRICT.district_id
-;
-
+SELECT SUM(LOAN.amount),
+       LOAN.start_date
+FROM dmelisso.LOAN LOAN
+         JOIN dmelisso.account ACCOUNT
+              on ACCOUNT.account_id = LOAN.account_id
+         JOIN dmelisso.DISTRICT_detail on DISTRICT_detail.district_id = ACCOUNT.district_id
+GROUP BY LOAN.start_date, DISTRICT_detail.district_id
+ORDER BY LOAN.start_date;
 
 -- Query 2: group the card by district and calculate the amount of card issued on each date in each district
-SELECT
-    COUNT(CARD.card_id)
-FROM
-    dmelisso.CARD
-    JOIN dmelisso.DISPOSITION on DISPOSITION.disp_id = CARD.disp_id
-    JOIN dmelisso.ACCOUNT on ACCOUNT.account_id = DISPOSITION.account_id
-    JOIN dmelisso.DISTRICT on DISTRICT.district_id = ACCOUNT.district_id
-GROUP BY
-    CARD.issued_date, DISTRICT.district_id
-;
+SELECT COUNT(CARD.card_id) AS number_of_cards
+FROM dmelisso.CARD
+         JOIN dmelisso.DISPOSITION
+              on DISPOSITION.disp_id = CARD.disp_id
+         JOIN dmelisso.ACCOUNT on ACCOUNT.account_id = DISPOSITION.account_id
+         JOIN dmelisso.DISTRICT_detail on DISTRICT_detail.district_id = ACCOUNT.district_id
+GROUP BY CARD.issued,
+         DISTRICT_detail.district_id;
