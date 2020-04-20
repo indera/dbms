f845c2b37ae9227f6664b5f372f087d9ecd02aac
Mukul Chand Yadav <mchand.yadav@ufl.edu>
2020-04-20 00:05:52 -0400

    Delete react-ui module
    - TODO Migrate to new charts

--- react-ui/src/modules/index.js
+++
@@ -1,9 +0,0 @@
-import AccountsDerivation from './Query1';
-import TransactionDerivation from './Query5';
-import Dashboard from './Dashboard';
-
-export default [
-    Dashboard,
-    AccountsDerivation,
-    TransactionDerivation,
-];
